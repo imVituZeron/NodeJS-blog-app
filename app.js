5a977128f96a14925f4364ea4e9eb932466d9e9f
vitor <vitorpaulasantos@gmail.com>
2019-10-09 16:05:03 -0300

    configurando o ambiente para o projeto

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const handlebars = require('express-handlebars');
+const bodyParser = require('body-parser');
+const app = express();
+//const mongoose = require('mongoose');
+
+// Configurações
+   //BodyParser
+      app.use(bodyParser.urlencoded({extended: true}));
+      app.use(bodyParser.json());
+   //handlebars
+      app.engine('handlebars', handlebars({defaultLayout: 'main'}));
+      app.set('view engine','handlebars');
+   //mongoose
+      // Em breve
+//
+// Rotas
+
+
+
+const PORT = 8081;
+app.listen(PORT, ()=> {
+   console.log('Server Online!');
+});
